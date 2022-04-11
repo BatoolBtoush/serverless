05996630edd10e8b88ca8cf3049e5874f7ac95c0
BatoolBtoush <batoolbtoush98@gmail.com>
2022-04-11 19:04:37 +0300

    image3

--- api/text.py
+++ api/text.py
@@ -24,10 +24,7 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.path = 'assets/time-pink-floyd.png'
-    content = open(self.path, 'rb').read()
     self.wfile.write(message)
-    self.wfile.write(content)
     
     
     return
