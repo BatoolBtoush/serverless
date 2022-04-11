5dd9ee8d6e33b1d40ab70c7f19d4845ba0582695
BatoolBtoush <batoolbtoush98@gmail.com>
2022-04-11 20:08:57 +0300

    mod

--- api/time.py
+++ api/time.py
@@ -15,8 +15,8 @@ class handler(BaseHTTPRequestHandler):
 
 
     first_message = f"\nGreetings from Python version {platform.python_version()}"
-    second_message = f"\This is the api/time page"
-    third_message = f"\nThis page will show the day, date, and time of today\n"
+    second_message = f"\nThis is the api/time page"
+    third_message = f"\nThis page will show the day, date, time, and year as of today\n"
     time_message = time.asctime(time.localtime(time.time()))
 
 
